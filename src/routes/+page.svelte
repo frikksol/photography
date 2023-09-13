7aa5cc3c3e3944b643db2dd6d7383f656efde2d2
Frikk Herding <frikksol@gmail.com>
2023-09-13 21:57:08 +0200

    💄 Use h1 for image subtitle

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,30 +26,32 @@
     <div class="px-2 pb-24 xl:px-16 xl:pb-36">
       <img src={image.url} alt={image.id} />
       <div class="flex">
-        {#if image.name}
-          Name: {image.name}
-        {/if}
-        {#if image.camera}
-          - Camera: {image.camera}
-        {/if}
-        {#if image.lens}
-          - Lens: {image.lens}
-        {/if}
-        {#if image.film}
-          - Film: {image.film}
-        {/if}
-        {#if image.shutter_speed}
-          - Shutter: {image.shutter_speed}
-        {/if}
-        {#if image.aperture}
-          - Aperture: {image.aperture}
-        {/if}
-        {#if image.exposure_compensation}
-          - Exposure Comp:{image.exposure_compensation}
-        {/if}
-        {#if image.width && image.height}
-          - Size: {image.width} x {image.height}
-        {/if}
+        <h1 class="pt-2">
+          {#if image.name}
+            Name: {image.name}
+          {/if}
+          {#if image.camera}
+            - Camera: {image.camera}
+          {/if}
+          {#if image.lens}
+            - Lens: {image.lens}
+          {/if}
+          {#if image.film}
+            - Film: {image.film}
+          {/if}
+          {#if image.shutter_speed}
+            - Shutter: {image.shutter_speed}
+          {/if}
+          {#if image.aperture}
+            - Aperture: {image.aperture}
+          {/if}
+          {#if image.exposure_compensation}
+            - Exposure Comp:{image.exposure_compensation}
+          {/if}
+          {#if image.width && image.height}
+            - Size: {image.width} x {image.height}
+          {/if}
+        </h1>
       </div>
     </div>
   {/each}
