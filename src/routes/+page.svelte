b33de6efda966ebabce18dbb40ceb8f311e0cc39
Frikk Herding <frikksol@gmail.com>
2023-09-13 21:40:36 +0200

    💬 Add an intro text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,9 +9,18 @@
 
 <div class="min-h-screen min-w-full">
   <div class="p-16 pb-36">
-    <h1 class="font-serif text-5xl font-bold text-black">
+    <h1 class="pb-24 font-serif text-5xl font-bold text-black">
       Frikk Herding Photography
     </h1>
+    <p class="max-w-6xl">
+      As someone who spends all day writing code; digital, immediate response
+      and always "perfect". Analog photography on on the other hand, is slow,
+      perfectly imperfect, and you there is no response except the click of the
+      shutter.
+      <br /> <br />
+      This is why I find this to be a great hobby. Below is a collection of some
+      of my photos.
+    </p>
   </div>
   {#each data?.images as image (image.id)}
     <div class="px-16 pb-36">
